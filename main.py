df21b5d496de9bd4b56470c49c3278ce8079501b
Lornatang <shiyipaisizuo@163.com>
2019-04-28 00:02:06 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from model import dnn
+
+from sklearn.datasets import  load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+
+X_data, y_data = load_breast_cancer(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, train_size=0.8, random_state=28)
+X_train = X_train.T
+y_train = y_train.reshape(y_train.shape[0], -1).T
+X_test = X_test.T
+y_test = y_test.reshape(y_test.shape[0], -1).T
+accuracy = dnn(X_train, y_train, X_test, y_test, [X_train.shape[0], 10, 5, 1], learning_rate=0.001, num_iterations=10000, beta1=0.9, beta2=0.999, epsilon=1e-8)
+print(accuracy)
