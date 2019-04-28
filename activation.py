da0aa76445eb1ba0f354c22a7c9b9bf4cb0be943
Lornatang <shiyipaisizuo@163.com>
2019-04-28 08:34:05 +0800

    Create activation.py

---
+++ activation.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+def relu(x):
+  """ linear activation function
+  Paras
+  -----------------------------------
+  x: output of the linear layer
+
+  Returns
+  -----------------------------------
+  max of nums
+  """
+  return np.maximum(0, x)
+
+
+def relu_backward(x):
+  """ derivation of relu
+  Paras
+  -----------------------------------
+  x: output of the linear layer
+
+  Returns
+  -----------------------------------
+  max of nums
+  """
+
+  return np.int64(x > 0)
+
+
+def sigmoid(x):
+  """implement the activation function sigmoid
+  Paras
+  -----------------------------------
+  x: Output of the linear layer
+  Returns
+  -----------------------------------
+  """
+
+  return 1 / (1 + np.exp(-x))
