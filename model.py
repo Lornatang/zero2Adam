6b376c7b7f142c1875ae2c35be847e0514976463
Lornatang <shiyipaisizuo@163.com>
2019-05-05 13:02:20 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,6 +7,7 @@
 ####################################################
 
 from ops import *
+from matplotlib import pyplot as plt
 
 
 def model(data,
@@ -14,78 +15,75 @@ def model(data,
           layer_dims,
           learning_rate,
           num_iterations,
-          beta1=0.9,
+          beta=0.9,
           beta2=0.999,
-          mini_batch_size=64,
           epsilon=1e-8):
-  """ define basic model
-  Paras
-  -----------------------------------
-  data:            input data, of shape (input size, number of examples)
-  label:           true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-  layer_dims:      list containing the input size and each layer size
-  learning_rate:   the learning rate, scalar
-  num_iterations:  number of iterative training
-  beta1:           Exponential decay hyperparameter for the first moment estimates
-  beta2:           Exponential decay hyperparameter for the second moment estimates
-  mini_batch_size: size of the mini-batches, integer
-  epsilon:         hyperparameter preventing division by zero in Adam updates
+    """ define basic model
+    Paras
+    -----------------------------------
+    data:            input data, of shape (input size, number of examples)
+    label:           true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    layer_dims:      list containing the input size and each layer size
+    learning_rate:   the learning rate, scalar
+    num_iterations:  number of iterative training
+    mini_batch_size: size of the mini-batches, integer
 
-  Returns:
-  -----------------------------------
-  paras：      final paras:(W,b)
-  """
-  costs = []
-  # initialize paras
-  paras = init_paras(layer_dims)
-  v, s = initialize_adam(paras)
-  t = 0  # initializing the counter required for Adam update
-  seed = 0
-  for i in range(0, num_iterations):
-    # Define the random minibatches. We increment the seed to reshuffle differently the dataset after each epoch
-    seed = seed + 1
-    minibatches = random_mini_batches(data, label, mini_batch_size)
-    for mini_batch in minibatches:
-      # Select a mini_batch
-      (mini_batch_X, mini_batch_Y) = mini_batch
-      # Forward propagation
-      AL, caches = forward_propagation(mini_batch_X, paras)
-      # Compute cost
-      loss = compute_loss(AL, mini_batch_Y)
-      # Backward propagation
-      grads = backward_propagation(AL, mini_batch_Y, caches)
-      t += 1
-      paras = update_parameters_with_adam(paras, grads, v, s, t, learning_rate, beta1, beta2, epsilon)
+    Returns:
+    -----------------------------------
+    paras：      final paras:(W,b)
+    """
+    global loss
+    losses = []
+    # initialize paras
+    paras = init_paras(layer_dims)
+    v, s = initialize_adam(paras)
+    t = 0
+    for i in range(0, num_iterations):
+        # Define the random mini batches. We increment the seed to reshuffle differently the dataset after each epoch
+        # Forward propagation
+        pred, caches = forward_propagation(data, paras)
+        # Compute cost
+        loss = compute_loss(pred, label)
+        # Backward propagation
+        grads = backward_propagation(pred, label, caches)
+        # update parameters
+        t += 1
+        paras = update_parameters_with_adam(paras, grads, v, s, t, learning_rate, beta, beta2, epsilon)
 
-      print(f"Iter {i} loss {loss:.6f}")
-      costs.append(loss)
-
-  return paras
+        if i % 200 == 0:
+            print(f"Iter {i} loss {loss:.6f}")
+            losses.append(loss)
+    plt.clf()
+    plt.plot(losses)  # o-:圆形
+    plt.xlabel("iterations(thousand)")  # 横坐标名字
+    plt.ylabel("cost")  # 纵坐标名字
+    plt.show()
+    return paras
 
 
 def predict(data, label, paras):
-  """predict function
-  Paras
-  -----------------------------------
-  data:            input data, of shape (input size, number of examples)
-  label:           true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-  parameter:       final parameters:(W,b)
+    """predict function
+    Paras
+    -----------------------------------
+    data:            input data, of shape (input size, number of examples)
+    label:           true "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    parameter:       final parameters:(W,b)
 
-  Returns
-  -----------------------------------
-  accuracy:        the correct value of the prediction
-  """
-  pred = np.zeros((1, label.shape[1]))
-  prob, caches = forward_propagation(data, paras)
-  for i in range(prob.shape[1]):
-    # Convert probabilities A[0,i] to actual predictions p[0,i]
-    if prob[0, i] > 0.5:
-      pred[0, i] = 1
-    else:
-      pred[0, i] = 0
-  accuracy = 1 - np.mean(np.abs(pred - label))
+    Returns
+    -----------------------------------
+    accuracy:        the correct value of the prediction
+    """
+    pred = np.zeros((1, label.shape[1]))
+    prob, _ = forward_propagation(data, paras)
+    for i in range(prob.shape[1]):
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        if prob[0, i] > 0.5:
+            pred[0, i] = 1
+        else:
+            pred[0, i] = 0
+    accuracy = 1 - np.mean(np.abs(pred - label))
 
-  return accuracy
+    return accuracy
 
 
 def dnn(X_train,
@@ -93,40 +91,29 @@ def dnn(X_train,
         X_test,
         y_test,
         layer_dims,
-        learning_rate=0.0005,
-        num_iterations=10000,
-        beta1=0.9,
-        beta2=0.999,
-        mini_batch_size=64,
-        epsilon=1e-8):
-  """DNN model
-   Paras
-  -----------------------------------
-  X_train:         train data, of shape (input size, number of examples)
-  y_train:         train "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-  X_test:          test data, of shape (input size, number of examples)
-  y_test:          test "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
-  layer_dims:      list containing the input size and each layer size
-  learning_rate:   the learning rate, scalar
-  num_iterations:  number of iterative training
-  beta1:           exponential decay hyperparameter for the first moment estimates
-  beta2:           exponential decay hyperparameter for the second moment estimates
-  mini_batch_size: size of the mini-batches, integer
-  epsilon:         hyperparameter preventing division by zero in Adam updates
+        learning_rate,
+        num_iterations):
+    """DNN model
+     Paras
+    -----------------------------------
+    X_train:         train data, of shape (input size, number of examples)
+    y_train:         train "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    X_test:          test data, of shape (input size, number of examples)
+    y_test:          test "label" vector (1 for blue dot / 0 for red dot), of shape (1, number of examples)
+    layer_dims:      list containing the input size and each layer size
+    learning_rate:   the learning rate, scalar
+    num_iterations:  number of iterative training
+    mini_batch_size: size of the mini-batches, integer
 
-  Returns
-  -----------------------------------
-  accuracy:        the correct value of the prediction
-  """
-  paras = model(X_train,
-                y_train,
-                layer_dims,
-                learning_rate,
-                num_iterations,
-                beta1,
-                beta2,
-                mini_batch_size,
-                epsilon)
-  accuracy = predict(X_test, y_test, paras)
+    Returns
+    -----------------------------------
+    accuracy:        the correct value of the prediction
+    """
+    paras = model(X_train,
+                  y_train,
+                  layer_dims,
+                  learning_rate,
+                  num_iterations)
+    accuracy = predict(X_test, y_test, paras)
 
-  return accuracy
+    return accuracy
